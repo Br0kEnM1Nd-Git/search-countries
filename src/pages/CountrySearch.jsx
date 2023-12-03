caa5684d28f3d26d9466d6da3bd8f0f3b39def7b
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-12-03 15:32:26 +0100

    task-3

--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,41 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const [countryList, setCountryList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const region = searchParams.get('query');
+    if (!region) return;
+    const getByRegion = async () => {
+      setError(null);
+      setLoader(true);
+      try {
+        const resp = await fetchByRegion(region);
+        setCountryList(resp);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+    getByRegion();
+  }, [searchParams]);
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={setSearchParams} />
+        {error && <Heading>error</Heading>}
+        {!error && <CountryList countries={countryList} />}
+        {loader && <Loader />}
       </Container>
     </Section>
   );
